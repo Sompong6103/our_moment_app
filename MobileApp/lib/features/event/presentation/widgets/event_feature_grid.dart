11dbb3840c3cceddd4528b7d521781a747733096
Sompong <sm.cheewa@gmail.com>
2026-04-06 22:23:22 +0700

    add: intergrate to detail page

--- MobileApp/lib/features/event/presentation/widgets/event_feature_grid.dart
+++ MobileApp/lib/features/event/presentation/widgets/event_feature_grid.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import '../../../../core/theme/app_colors.dart';
+import '../pages/live_gallary.dart';
 
 class EventFeatureGrid extends StatelessWidget {
   const EventFeatureGrid({super.key});
@@ -28,7 +29,15 @@ class EventFeatureGrid extends StatelessWidget {
                 iconColor: AppColors.primary,
                 title: 'Live Gallery',
                 subtitle: 'Real-time photos',
-                onTap: () {},
+                onTap: () {
+                  // Navigate to live gallery screen
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                      builder: (context) => LiveGalleryScreen(),
+                    ),
+                  );
+                },
               ),
             ),
           ],
